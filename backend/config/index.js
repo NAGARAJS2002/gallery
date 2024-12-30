b9b1f9751430a3bd041bd6a207c0b670710045a9
NAGARAJS2002 <nr2277644@gmail.com>
2024-12-30 11:51:10 +0530

    complete user authentication

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,3 +2,4 @@ import env from "dotenv";
 env.config();
 
 export const mongoURI = process.env.MONGO_URI;
+export const JWT_SECRET = process.env.JWT_SECRET;
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,8 @@
 import {User} from "../models/index.js"
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
+import { JWT_SECRET } from "../config/index.js";
+import jwt from "jsonwebtoken"
 export const register = async (req,res,next) => {
     const {username,email,password} = req.body;
        if (!username || !email || !password) {
@@ -8,7 +11,7 @@ export const register = async (req,res,next) => {
     try {
 
         const hashedPassword = await bcryptjs.hashSync(password, 10);
-        
+
         await User.create({
             username: username,
             email: email,
@@ -19,4 +22,27 @@ export const register = async (req,res,next) => {
     } catch (error) {
         next(error)
     }   
+}
+
+export const login  = async (req,res,next) => {
+    const {email , password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) {
+            return next(errorHandler(404,'Invalid Credentials'))
+        }
+        const validPassword = await bcryptjs.compareSync(password,validUser.password);
+        if (!validPassword) return next(errorHandler(404,'Wrong Credentials'));
+
+        if (JWT_SECRET == undefined) {
+            return next(errorHandler('JWT_SECRET is not defined'))
+        };
+        jwt.sign({_id:validUser.id},JWT_SECRET,{expiresIn:3600},(err, token) => {
+            if (err) throw err;
+            res.cookie('access_token',token,{httpOnly:true}).status(201).json(validUser);
+        })
+
+    } catch (error) {
+        
+    }
 }
